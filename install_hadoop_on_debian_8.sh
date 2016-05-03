7025f8a41570000845ab683c3ee12693468382fa
Qiang Zhang <zhangtemplar@users.noreply.github.com>
2016-05-02 22:37:36 -0700

    Update install_hadoop_on_debian_8.sh

--- install_hadoop_on_debian_8.sh
+++ install_hadoop_on_debian_8.sh
@@ -1,3 +1,16 @@
+if ["$#" -ne 1]; then
+	echo 'type: datanode'
+	type=datanode
+else
+	if "$1" = namenode then
+		echo 'type: namenode'
+		type=namenode
+	else
+		echo 'type: datanode'
+		type=datanode
+	fi
+fi
+
 echo 'install java'
 apt-get update
 apt-get install -y openjdk-7-jdk
@@ -9,8 +22,7 @@ tar xvf hadoop-* -C /usr/local
 mv /usr/local/hadoop-* /usr/local/hadoop
 
 echo 'configure hadoop'
-apt-get install vim
-mkdir -p $HADOOP_HOME/hadoop_data/hdfs/namenode
+# apt-get install vim
 tar -xf conf.tar
 cp -f conf/.profile .profile
 cp -f conf/hadoop-env.sh $HADOOP_CONF_DIR/hadoop-env.sh
@@ -18,8 +30,16 @@ cp -f conf/core-site.xml $HADOOP_CONF_DIR/core-site.xml
 cp -f conf/yarn-site.xml $HADOOP_CONF_DIR/yarn-site.xml
 cp -f conf/mapred-site.xml $HADOOP_CONF_DIR/mapred-site.xml
 cp -f conf/hosts /etc/hosts
-cp -f conf/slaves $HADOOP_CONF_DIR/slaves
-cp -f conf/masters $HADOOP_CONF_DIR/masters
+# only required for namenode
+if "$type" = namenode then
+	cp -f conf/slaves $HADOOP_CONF_DIR/slaves
+	cp -f conf/masters $HADOOP_CONF_DIR/masters
+	cp -f conf/hdfs-site-namenode.xml $HADOOP_CONF_DIR/hdfs-site.xml
+	mkdir -p $HADOOP_HOME/hadoop_data/hdfs/namenode
+else
+	cp -f conf/hdfs-site-datanode.xml $HADOOP_CONF_DIR/hdfs-site.xml
+	mkdir -p $HADOOP_HOME/hadoop_data/hdfs/datanode
+fi
 rm -rf conf
 
 echo 'change password of root'
@@ -32,4 +52,3 @@ echo 'change permission of hadoop home user'
 chown -R zhangtemplar $HADOOP_HOME
 
 echo 'one more thing: please change your hostname according to'
-cat /etc/hosts
